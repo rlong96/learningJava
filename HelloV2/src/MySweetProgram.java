b51660d2b98c7fb9274fcc0ded7bcf1d1b63f1fa
Ryan Long <ryanlong@Ryans-MacBook-Pro.local>
2019-12-12 12:55:48 -0600

    initial commit

---
+++ HelloV2/src/MySweetProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Enter something:");
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		System.out.println(s);
+	}
+
+}
